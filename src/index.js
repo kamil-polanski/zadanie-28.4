486e1c7e3b22e615cb4bb3324d05b2f15e49c5b5
Kamil Polanski <polanski88@gmail.com>
2019-10-09 15:30:38 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import DevTools from './DevTools';
+import {
+	getCountries
+} from './actions/actions';
+
+render(
+  <Provider store={store}>
+    <div>
+      <h1>Inicjalizacja projektu</h1>
+      <DevTools />
+    </div>
+
+  </Provider>,
+  document.getElementById('root')
+);
+
+store.dispatch(getCountries());
