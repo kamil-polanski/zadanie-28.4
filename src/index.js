83575147bd86196265fd313bcaaa3cf41268245e
Kamil Polanski <polanski88@gmail.com>
2019-10-09 16:00:45 +0200

    add navigation, routing

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import DevTools from './DevTools';
-import {
-	getCountries
-} from './actions/actions';
+import { getCountries } from './actions/actions';
+import routes from './routes';
 
 render(
   <Provider store={store}>
     <div>
       <h1>Inicjalizacja projektu</h1>
+      <Router history={hashHistory} routes={routes} />
       <DevTools />
     </div>
 
